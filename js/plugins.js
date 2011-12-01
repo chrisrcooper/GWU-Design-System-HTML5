239089a0521df9d3514f60d2e7e85872765b6618
Christopher Cooper <Chris@Chris-MacBook-Air.local>
2011-12-01 10:38:22 -0500

    Version 1

---
+++ js/plugins.js
@@ -0,0 +1,50 @@
+
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+    arguments.callee = arguments.callee.caller;
+    var newarr = [].slice.call(arguments);
+    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+  }
+};
+
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
+
+
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+/*-------------------------------------------------------------------- 
+ * JQuery Plugin: "EqualHeights"
+ * by:	Scott Jehl, Todd Parker, Maggie Costello Wachs (http://www.filamentgroup.com)
+ *
+ * Copyright (c) 2008 Filament Group
+ * Licensed under GPL (http://www.opensource.org/licenses/gpl-license.php)
+ *
+ * Description: Compares the heights or widths of the top-level children of a provided element 
+ 		and sets their min-height to the tallest height (or width to widest width). Sets in em units 
+ 		by default if pxToEm() method is available.
+ * Dependencies: jQuery library, pxToEm method	(article: 
+		http://www.filamentgroup.com/lab/retaining_scalable_interfaces_with_pixel_to_em_conversion/)							  
+ * Usage Example: $(element).equalHeights();
+  		Optional: to set min-height in px, pass a true argument: $(element).equalHeights(true);
+ * Version: 2.0, 08.01.2008
+--------------------------------------------------------------------*/
+
+$.fn.equalHeights = function(px) {
+	$(this).each(function(){
+		var currentTallest = 0;
+		$(this).children().each(function(i){
+			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+		});
+		//if (!px || !Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
+		// for ie6, set height since min-height isn't supported
+		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'height': currentTallest}); }
+		$(this).children().css({'min-height': currentTallest}); 
+	});
+	return this;
+};
---
+++ js/script.js
@@ -0,0 +1,89 @@
+/* Author: cooper@gwu.edu
+
+*/
+
+$(document).ready(function()
+{
+	/*
+	 * give elements max height of tallest child
+	 */
+	$('div.summary-container').equalHeights();
+	$('section.selector-links div.link-container').equalHeights();
+	
+	/*
+	 * search clear and replace
+	 */
+	$('#search input').focus(
+		function(){
+		search_value = $(this).attr('value');
+		$(this).attr('value','');			
+	}).blur(
+		function(){					
+		if($(this).val() == "")	
+			$(this).attr('value',''+search_value+'');
+	}); 
+	
+	/*
+	 * featured carousel
+	 */
+	$('div.slide:not(selected)').click(function()
+	{
+		$(this).siblings('.selected').removeClass('selected');
+		$(this).addClass('selected');	
+		var slide_clicked = $(this).index()+1;
+		
+		$('div.summary-container .summary.selected').removeClass('selected');
+		$('div.summary-container .summary:nth-child('+slide_clicked+')').addClass('selected');
+					
+	});
+	
+	/*
+	 * selector links
+	 */
+	$('section.selector-links select').change(function()
+	{
+		var option_clicked = $(this).children(':selected').index()+1;
+		
+		$(this).next().children('ul:visible').removeClass('selected');
+		$(this).next().children('ul:nth-child('+option_clicked+')').addClass('selected');
+	});
+	
+	/*
+	 * local header
+	 */
+	$('div.hero-container ul.menu li:not(.selected)').live("mouseenter",function(){
+		var menu_hovered = $(this).index()+1;
+		
+		//reset the menu
+		$(this).siblings().removeClass('selected');
+		$(this).addClass('selected');
+		
+		//set the image
+		$(this).parent().siblings('div.images').children(':visible').removeClass('selected').siblings(':nth-child('+menu_hovered+')').addClass('selected');
+		
+		//set the caption
+		$(this).parent().siblings('ul.captions').children(':visible').removeClass('selected').siblings(':nth-child('+menu_hovered+')').addClass('selected');
+	});
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
